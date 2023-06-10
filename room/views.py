07f11f81353f7353288174bea514b48d503ddd7d
SkofiTI <cactus.master994@gmail.com>
2023-06-10 16:18:50 +0300

    Update room templates

--- room/views.py
+++ room/views.py
@@ -11,12 +11,7 @@ def rooms_list(request):
 
 def room_detail(request, slug):
     room = get_object_or_404(Room, slug__iexact=slug)
-    months = room.months.all()
-    context = {
-        'room': room,
-        'months': months,
-    }
-    return render(request, 'room/pages/room_detail.html', context=context)
+    return render(request, 'room/pages/room_detail.html', context={'room': room})
 
 
 def profile(request):
